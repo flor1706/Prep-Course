e65277b4ca5c82cdf3d04f66ebbe8fc5d26f1299
flor1706 <107158290+flor1706@users.noreply.github.com>
2022-07-20 19:07:58 -0300

    Add files via upload

---
+++ OB-EJERCICIO-1-PYTHON.py
@@ -0,0 +1,8 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print("Hola soy Florencia")
+Hola soy Florencia
+print("estoy empezando el curso de python")
+estoy empezando el curso de python
+print("Espero aprender mucho")
+Espero aprender mucho
